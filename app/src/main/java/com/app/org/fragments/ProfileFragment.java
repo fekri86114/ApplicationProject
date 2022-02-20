8c92ebd543f857c917c4c96efac6b901b5c1d6f7
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-02-20 20:17:52 +0330

    added menu items

--- app/src/main/java/com/app/org/fragments/ProfileFragment.java
+++ app/src/main/java/com/app/org/fragments/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.app.org;
+package com.app.org.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.app.org.R;
 
 /**
  * A simple {@link Fragment} subclass.
