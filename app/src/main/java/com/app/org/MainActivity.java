0039afbf7606ae75c90a80f34d79c7c4cfe8f325
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-02-22 21:54:05 +0330

    complete app bottom navigation

--- app/src/main/java/com/app/org/MainActivity.java
+++ app/src/main/java/com/app/org/MainActivity.java
@@ -1,8 +1,17 @@
 package com.app.org;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+
+import androidx.navigation.Navigation;
+
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+//
+//        navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> {
+//            int id = navDestination.getId();
+//
+//            switch (id) {
+//                case R.id.nav_home:
+//                    Toast.makeText(MainActivity.this, "HOME", Toast.LENGTH_LONG).show();
+//                    break;
+//                case R.id.nav_profile:
+//                    Toast.makeText(MainActivity.this, "PROFILE", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case R.id.nav_settings:
+//                    Toast.makeText(MainActivity.this, "SETTING", Toast.LENGTH_SHORT).show();
+//                    break;
+//                case R.id.nav_about_us:
+//                    Toast.makeText(MainActivity.this, "ABOUT US", Toast.LENGTH_SHORT).show();
+//                    break;
+//            }
+//        });
     }
 }
