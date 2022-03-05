7896964c6977fea388b7d288a68c5b4a2ac8069e
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-03-05 22:40:18 +0330

    added toggle and drawer complete

--- app/src/main/java/com/app/org/MainActivity.java
+++ app/src/main/java/com/app/org/MainActivity.java
@@ -1,31 +1,67 @@
 package com.app.org;
 
-
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
-
 import androidx.navigation.Navigation;
-
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    public NavigationView navigationView;
+    public Toolbar toolbar;
+    public DrawerLayout drawerLayout;
+    public NavController navController;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        navController = Navigation.findNavController(this, R.id.nav_fragment);
+        setupBottomNav();
+        setupDrawerNav();
 
         navController.addOnDestinationChangedListener((navController1, navDestination, bundle) -> {
-//            int id = navDestination.getId();
+
+        });
+    }
+
+    private void setupDrawerNav() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        drawerToggle = setupDrawerToggle();
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
+        drawerLayout.addDrawerListener(drawerToggle);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setupBottomNav() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        //            int id = navDestination.getId();
 //
 //            switch (id) {
 //                case R.id.nav_home:
@@ -41,6 +77,59 @@ public class MainActivity extends AppCompatActivity {
 //                    Toast.makeText(MainActivity.this, "ABOUT US", Toast.LENGTH_SHORT).show();
 //                    break;
 //            }
-        });
+    }
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        drawerLayout.closeDrawers();
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                navController.navigate(R.id.nav_home);
+                break;
+
+            case R.id.nav_profile:
+                navController.navigate(R.id.nav_profile);
+                break;
+
+            case R.id.nav_settings:
+                navController.navigate(R.id.nav_settings);
+                break;
+            case R.id.nav_about_us:
+                navController.navigate(R.id.nav_about_us);
+                break;
+        }
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
